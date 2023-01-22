bf7cc5e6bca5d0256240ac47fea5e99d15014d7a
Wesley <wesley55daniel@gmail.com>
2023-01-22 18:49:34 -0300

    Adicionando campo data de nascimento

--- projetoJava1/src/curso/java/pessoa/Pessoa.java
+++ projetoJava1/src/curso/java/pessoa/Pessoa.java
@@ -2,36 +2,41 @@ package curso.java.pessoa;
 
 public class Pessoa {
 
-	
 	private String nome;
 	private String sobrenome;
 	private int idade;
 	private String endereco;
-	
-	
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getSobrenome() {
 		return sobrenome;
 	}
+
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
+
 	public int getIdade() {
 		return idade;
 	}
+
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-	
-	
-	
-	
-	
-	
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
 }
