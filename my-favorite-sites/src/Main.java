043dae9c8b05f38d8d1ad22225631283945beb14
Wesley <wesley55daniel@gmail.com>
2023-01-31 00:17:25 -0300

    Atualização 1

---
+++ my-favorite-sites/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String [] args) {
+		System.out.println("---------------- MEUS SITES FAVORITOS ---------------- ");
+		
+		Scanner scanner = new Scanner (System.in);
+		List <Website> meusWebsites = new ArrayList();
+		
+		
+		String nome = scanner.nextLine();
+		String url = scanner.nextLine();
+		
+		Website website = new Website();
+		
+		website.setNome(nome);
+		website.setUrl(url);
+		}
+	
+}
