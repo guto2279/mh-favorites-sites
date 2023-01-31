2e2ecec9f07010d7190104a5170a3ffc71cf3894
wesle <wesle@Prog-PC>
2023-01-31 13:13:46 -0300

    Permitir cadastro de sites

--- my-favorite-sites/src/Main.java
+++ my-favorite-sites/src/Main.java
@@ -8,16 +8,24 @@ public class Main {
 		System.out.println("---------------- MEUS SITES FAVORITOS ---------------- ");
 		
 		Scanner scanner = new Scanner (System.in);
-		List <Website> meusWebsites = new ArrayList();
+		List <Website> meusWebsites = new ArrayList<>();
 		
-		
+		System.out.println("Digite o nome do Website");
 		String nome = scanner.nextLine();
+		System.out.println("Digite o link do Website");
 		String url = scanner.nextLine();
 		
 		Website website = new Website();
 		
 		website.setNome(nome);
 		website.setUrl(url);
+		
+		meusWebsites.add(website);
+		
+		for (Website websites : meusWebsites) {
+			System.out.println(websites);
+		}
+		
 		}
 	
 }
