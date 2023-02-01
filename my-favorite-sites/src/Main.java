94c7eb241fdd7c838112cc7da5c7cb13af966c6a
Wesley <wesley55daniel@gmail.com>
2023-02-01 00:10:27 -0300

    Colocando meu em um laço de repetição do-while

--- my-favorite-sites/src/Main.java
+++ my-favorite-sites/src/Main.java
@@ -10,21 +10,31 @@ public class Main {
 		Scanner scanner = new Scanner (System.in);
 		List <Website> meusWebsites = new ArrayList<>();
 		
-		System.out.println("Digite o nome do Website");
-		String nome = scanner.nextLine();
-		System.out.println("Digite o link do Website");
-		String url = scanner.nextLine();
+		String sair = "n";
 		
-		Website website = new Website();
+		do {
+			System.out.println("Digite o nome do Website");
+			String nome = scanner.nextLine();
+			System.out.println("Digite o link do Website");
+			String url = scanner.nextLine();
+			
+			Website website = new Website();
+			
+			website.setNome(nome);
+			website.setUrl(url);
+			
+			System.out.println("Deseja sair?");
+			sair = scanner.nextLine();
+			meusWebsites.add(website);
+			
+		} while (!sair.equals("s"));
 		
-		website.setNome(nome);
-		website.setUrl(url);
-		
-		meusWebsites.add(website);
+	System.out.println("----------------- Lista de sites -----------------");
 		
 		for (Website websites : meusWebsites) {
 			System.out.println(websites);
 		}
+	System.out.println("--------------------------------------------------");	
 		
 		}
 	
